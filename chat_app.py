5c2bf98f9218945e682de39e9c2f45c69265ab57
Nwachuku Joshua <nwachukujosh2000@gmil.com>
2025-07-20 10:20:57 +0100

    main

---
+++ chat_app.py
@@ -0,0 +1,10 @@
+import gradio as gr
+from transformers import pipeline
+
+chatbot = pipeline("text-generation", model="distilgpt2")
+
+def chat(prompt):
+    response = chatbot(prompt, max_length=50)[0]['generated_text']
+    return response
+
+gr.Interface(fn=chat, inputs="text", outputs="text", title="Mini LLM Chat").launch()
